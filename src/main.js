21b1b86b651bd403087bb92a99c09df93d85846c
Ueslei Amaral <uespamaral@gmail.com>
2023-05-21 06:59:24 -0300

    finalização da seção habilidades e inicio da seção projetos

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import { animate } from "./animate.js";
 
 const home = document.querySelector(".home");
 const about = document.querySelector(".about");
+const projects = document.querySelector(".projects");
+
 
 animate(home);
 animate(about);
+
+animate(projects);
+
+projects.forEach((project) => {
+
+});
