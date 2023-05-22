070bc73a75902d29417f4bd14f29c4270197c770
Ueslei Amaral <uespamaral@gmail.com>
2023-05-22 01:13:30 -0300

    finalização do footer e seção de contato

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import { animate } from "./animate.js";
 const home = document.querySelector(".home");
 const about = document.querySelector(".about");
 const projects = document.querySelector(".projects");
+const contact = document.querySelector(".contact");
 
 
 animate(home);
 animate(about);
 
 animate(projects);
-
-projects.forEach((project) => {
-
-});
+animate(contact);
